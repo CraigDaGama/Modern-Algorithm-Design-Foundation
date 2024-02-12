42f24093893e3c6a7aed56b56010a65fdec69f1a
Craig <craigdagama@gmail.com>
2024-02-12 23:44:08 +0530

    Add files via upload

---
+++ PatternMaking.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int Patternmatching(string text, string pattern, int n, int m)
+{   int flag = 0;
+    for (int i=0; i< n-m ;i++)
+    {
+        int j=0;
+        while(j<m && pattern[j]==text[i+j])
+            j++;
+        if (j==m){
+         cout<<"pattern found at "<<i+1<<endl;
+         flag++;
+        }
+
+
+    }
+    if(flag == 0)
+        cout<<"pattern not found"<<endl;
+}
+int main()
+{
+    int n,m,p;
+    string text,pattern;
+    cout<<"Enter some text"<<endl;
+    cin>>text;
+    cout<<"Enter pattern to be found"<<endl;
+    cin>>pattern;
+
+    n=text.size();
+    m=pattern.size();
+    p=Patternmatching(text, pattern, n, m);
+
+    return 0;
+}
